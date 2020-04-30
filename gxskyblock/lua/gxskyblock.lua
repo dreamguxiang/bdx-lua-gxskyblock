892ca57e9e5f3e781b98725cea4015e9955669e1
dreamguxiang <you@example.com>
2020-04-30 23:10:51 +0800

    skyblock

--- gxskyblock/lua/gxskyblock.lua
+++ gxskyblock/lua/gxskyblock.lua
@@ -1,34 +1,36 @@
-function FileRead()
+----------------------打开文件----------------------------
+function FileRead()--暂时无用
     local file = io.open("gxPlugin/gxskyblock/skyblcoklist.json","r")
     local json = file:read("*a")
     file:close()
     return json
 end
-function skyx()
+function skyx()--获取x
     local file = io.open("gxPlugin/gxskyblock/local/x","r")
     local skyx = file:read("*a")
     file:close()
     return skyx
 end
-function skyy()
+function skyy()--获取y
     local file = io.open("gxPlugin/gxskyblock/local/y","r")
     local skyy = file:read("*a")
     file:close()
     return skyy
 end
-function skyz()
+function skyz()--获取z
     local file = io.open("gxPlugin/gxskyblock/local/z","r")
     local skyz = file:read("*a")
     file:close()
     return skyz
 end
-function clone()
+-----------------------------创建空岛------------------------
+function clone()--创建空岛
     local x = skyx()
     local y = skyy()
     local z = skyz()
     runCmd('clone -8 0 7 5 16 -5 '..x..' '..y..' '..z..'')
 end
-event.Filter("CMD","kd create",true,function(name,a)
+event.Filter("CMD","kd create",true,function(name,a)--创岛
     if dget(name,"dream1") == "true" then
         sendText(name,"§c你已经领过岛屿或者已加入他人岛屿")
         else
@@ -64,7 +66,8 @@ event.Filter("CMD","kd create",true,function(name,a)
     end
 return -1
 end)
-event.Filter("CMD","kd del",true,function(name,a)
+--------------------------------------删除空岛-----------------------------------
+event.Filter("CMD","kd del",true,function(name,a)--删岛
     if dget(name,"dream1") == "true" then
         sendText(name,"§6你确认要删除你的空岛吗，确定输入/kd confirm，取消输入/kd exit，你一共有15秒的时间考虑！")
         dput(name,"confirm1","true")
@@ -75,13 +78,13 @@ event.Filter("CMD","kd del",true,function(name,a)
     end
 return -1
 end)
-function delconfirm()
-    if dget(name,"confirm1") == "true" then
+function delconfirm()--超时处理
+    if dget(kdkdname,"confirm1") == "true" then
         dput(kdkdname,"confirm1",false)
         sendText(kdkdname,"§c请求超时")
     end
 end
-function scxt(name,cmd)
+function scxt(name,cmd)--删岛确认
     if dget(name,"confirm1") == "true" then
         if cmd == "kd confirm" then
             sendText(name,"§6删除空岛成功！")
@@ -99,4 +102,60 @@ function scxt(name,cmd)
         end
     end
 end
-Listen("onCMD","scxt")
+Listen("onCMD","scxt")--超时系统
+function delplayertrust()
+    if dget(ttwjplayer,"playertrust") == "true" then
+       dput(ttwjplayer,"playertrust","false")
+       sendText(trustowners,'§c请求超时')
+       sendText(ttwjplayer,'§c请求超时')
+    end
+end
+--------------------------------给予选项系统----------------------------------
+local onplayers = oList()--获取在线玩家
+event.Filter("CMD","kd trust",true,function(name,a)--信任玩家
+    trustowners = name
+    if string.find(onplyaers,a,1) ~= nil then
+        sendText(a,'§6玩家'..name..'邀请你加入他的岛！接受:/kd accppt,拒绝:/kd deny，你一共有15秒的时间考虑') 
+        schedule("delplayertrust",0,30)
+        trustplayer()      
+    else
+        sendText(name,'§c玩家不存在或离线！')
+    end
+return -1
+end)
+function trustwj()--打开主人json
+    local file = io.open('gxPlugin/SKW/'..trustowners..'-'..trustowners..'.json',"r")
+    local json = file:read("*a")
+    file:close()
+    return json
+end
+function ttwj(name,cmd)--确认系统
+    ttwjplayer = name
+    dput(name,"playertrust","true")
+    if dget(name,"playertrust") == "true" then
+        if cmd == "kd accept" then
+            sendText(name,'§a成功拒绝来自§5'..trustowners..'§a邀请！')
+            dput(name,"playertrust","false")
+            runCmdAs(trustowners,'land trust '..name..'')
+            local cj =require("cjson")
+            local fileconfig = trustwj()
+            local json = cj.decode(fileconfig)
+            for de,ed in ipairs(json.local) do
+            file = io.open('gxPlugin/gxskyblock/players/'..trustowner..'-'..name..'.json',"w+")
+            local msg = string.format('{\n\"name\":["%s"],\n\"local\":["%s"]\n\"owners\":["%s"]\n}',name,ed,trustowners)
+            file:write(msg)
+            file:close()
+            return -1
+            end
+        else
+           if cmd == "kd deny" then
+                sendText(name,'§c成功拒绝来自§5'..trustowners..'§c邀请！')
+                sendText(trustowners,'§c玩家§5'..name..'§c已拒绝你的邀请')
+                dput(name,"playertrust","false")
+                return -1
+            end
+        end
+    end
+end
+Listen("onCMD","ttwj")
+-------------------------移除权限系统--------------------------------
